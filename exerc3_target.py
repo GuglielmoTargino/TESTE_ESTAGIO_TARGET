176fd2e6651dc16ebbd502771e6a0409745272d0
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-27 19:13:50 -0300

    finalizacao_comentario
    Finalização e adição de comentários.

--- exerc3_target.py
+++ exerc3_target.py
@@ -1,28 +1,28 @@
 
 #Exercício para se calcular o maior faturamento de uma distribuidora. 
 
-bandeira='s'
-menor=maior=0
-ctor=soma=media=ctor2=0
+bandeira='s' # variável para sinalizar se o usuario deseja continuar com o cadastro
+menor=maior=0 # variáveis para guardar o maior e o menor valor registrado
+ctor=soma=media=ctor2=0 # variáveis para contar os dias e calcular a média
 
 while bandeira!='N':
 
     nr=float(input('Informe o valor do faturamento'))
     bandeira=str(input('Deseja continuar?_ [S/N]_')).upper().strip()[0]
-    if ctor==0:
+    if ctor==0: # mantem o menor valor no inicio.
         menor=nr
-    if nr>maior:
+    if nr>maior: # guarda o maior valor
         maior=nr
-    if nr<menor:
+    if nr<menor:# guarda o menor valor 
         menor=nr
-    ctor+=1
+    ctor+=1 #registra quntas vezes foram teve faturamento
     soma+=nr
     media=soma/ctor
     if nr>media:# linha para se testar quantos dias o faturamento ultrapassou a média;
         ctor2+=1
 
 print('O menor valor de faturamento foi R$={:.2f}. O maior valor de faturamento foi R$={:.2f}'.format(menor,maior))
-print(f"media{media}")
+
 print(f"O faturamento diário ultrapassou a média {ctor2} vezes este mês")
 
 
